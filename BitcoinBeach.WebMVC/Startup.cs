0f1d6d00b9b6e003955f57b9fe21719de1180eb3
Danny Redden <dannyredden18@gmail.com>
2022-02-08 19:32:05 -0500

    Add project files.

---
+++ BitcoinBeach.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BitcoinBeach.WebMVC.Startup))]
+namespace BitcoinBeach.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
