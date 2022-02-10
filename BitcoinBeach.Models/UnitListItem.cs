2ad20476e4531d9ecffb2a4b91c4b05397bef8d5
Danny Redden <dannyredden18@gmail.com>
2022-02-09 20:40:18 -0500

    added Unit controller, index view, UnitCreate class, Unit Create View, Unit Create Post method added. Adjusted Navbar to show Units

---
+++ BitcoinBeach.Models/UnitListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitcoinBeach.Models
+{
+    public class UnitListItem
+    {
+        public int UnitId { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ BitcoinBeach.WebMVC/Controllers/UnitController.cs
@@ -0,0 +1,37 @@
+﻿using BitcoinBeach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BitcoinBeach.WebMVC.Controllers
+{
+    [Authorize]
+    public class UnitController : Controller
+    {
+        // GET: Unit
+        public ActionResult Index()
+        {
+            var model = new UnitListItem[0];
+            return View(model);
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create (UnitCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
