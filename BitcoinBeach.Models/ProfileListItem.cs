8ad3ac6b1586e51d214061f01ebecefabfdf18c6
Danny Redden <dannyredden18@gmail.com>
2022-02-15 20:59:06 -0500

    added profile list item model and added a Index view for Accounts

--- BitcoinBeach.Models/ProfileListItem.cs
+++ BitcoinBeach.Models/ProfileListItem.cs
@@ -1,19 +1,21 @@
-﻿using BitcoinBeach.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BitcoinBeach.Data
+namespace BitcoinBeach.Models
 {
-    public class Profile
+    public class ProfileListItem
     {
         public Guid ProfileId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public ProfileType ProfileType { get; set; }
+
+        [Display(Name="Account Type")]
+        public ProfileType ProfileType {get; set; }
     }
 }
--- BitcoinBeach.WebMVC/Controllers/ProfileController.cs
+++ BitcoinBeach.WebMVC/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitcoinBeach.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,14 @@ using System.Web.Mvc;
 
 namespace BitcoinBeach.WebMVC.Controllers
 {
+    [Authorize]
     public class ProfileController : Controller
     {
         // GET: Profile
         public ActionResult Index()
         {
-            return View();
+            var model = new ProfileListItem[0];
+            return View(model);
         }
     }
 }
